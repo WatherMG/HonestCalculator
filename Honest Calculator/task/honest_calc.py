4184020bffc5a0db073a06433acdfdd21eba619e
WatherMG <dr.wather@gmail.com>
2021-12-04 10:42:35 +0300

    Initial commit Stage 1/5: Data collection

--- Honest Calculator/task/honest_calc.py
+++ Honest Calculator/task/honest_calc.py
@@ -1,1 +1,27 @@
-# write your code here
+import ast
+
+msg_0 = "Enter an equation"
+msg_1 = "Do you even know what numbers are? Stay focused!"
+msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+msg_3 = "Yeah... division by zero. Smart move..."
+
+valid_operations = ["+", "-", "/", "*"]
+running = True
+
+while running:
+    print(msg_0)
+    user_input = input()
+    x, operation, y = user_input.split()
+
+    try:
+        x = float(x)
+        y = float(y)
+    except ValueError:
+        print(msg_1)
+        continue
+
+    if operation not in valid_operations:
+        print(msg_2)
+        continue
+
+    running = False
